874fb1ac9e1059bcb0ebaa8d6902eb76a69546c0
Makarov1999 <makarov.e1999@yandex.ru>
2020-06-25 22:55:17 +0500

    Add new data

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 'use strict';
 
 (function () {
-  var photosCount = 25;
-
-  // var photos = window.data.generateRandomPhotos(photosCount);
   window.server.load('https://javascript.pages.academy/kekstagram/data', window.gallery.renderPhotos, function () {});
 
 })();
